33abe8a39a9f408313d166352af0ab35dab4aeef
Antonino Pagana <pagana899@gmail.com>
2024-03-28 17:39:22 +0100

    fix profumo classes / repo

--- src/main/java/com/team3/essence/lavib/essence_lab/controllers/ProfumoController.java
+++ src/main/java/com/team3/essence/lavib/essence_lab/controllers/ProfumoController.java
@@ -21,7 +21,7 @@ public class ProfumoController {
      * @return profumo aggiunto.
      */
     @PutMapping("/add")
-    public ResponseEntity<Profumo> addProfumo(@RequestBody Profumo profumo){
+    public ResponseEntity<Profumo> addProfumo(@RequestBody Profumo profumo) {
         Profumo profumoAdded = profumoService.addProfumo(profumo);
         return ResponseEntity.ok().body(profumoAdded);
     }
@@ -30,7 +30,7 @@ public class ProfumoController {
      * @return mostra la lista completa dei profumi.
      */
     @GetMapping("/viewProfumi")
-    public ResponseEntity<List<Profumo>> getListaProfumo (){
+    public ResponseEntity<List<Profumo>> getListaProfumo() {
         List<Profumo> profumoView = profumoService.getAllProfumi();
         return ResponseEntity.ok().body(profumoView);
     }
@@ -39,24 +39,24 @@ public class ProfumoController {
      * @param id con l'id cerca il profumo
      * @return mostra il profumo trovato o se non esiste mostra "not found".
      */
-    @GetMapping ("/profumo/{id}")
-    public ResponseEntity<Profumo> getProfumoId (@PathVariable Long id){
+    @GetMapping("/profumo/{id}")
+    public ResponseEntity<Profumo> getProfumoId(@PathVariable Long id) {
         Optional<Profumo> profumoOptional = profumoService.getProfumoId(id);
-        if (profumoOptional.isPresent()){
+        if (profumoOptional.isPresent()) {
             return ResponseEntity.ok().body(profumoOptional.get());
         }
         return ResponseEntity.notFound().build();
     }
 
     /**
-     * @param id con l'id cerca i negozi
+     * @param id      con l'id cerca i negozi
      * @param profumo
      * @return mostra il profumo aggiornato se c'è altrimenti mostra "not found"
      */
-    @PutMapping ("/update/{id}")
-    public ResponseEntity<Profumo> updateProfumo (@PathVariable Long id, @RequestBody Profumo profumo){
-        Optional <Profumo> profumoToUpdate = profumoService.updateProfumoId(id, profumo);
-        if (profumoToUpdate.isPresent()){
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Profumo> updateProfumo(@PathVariable Long id, @RequestBody Profumo profumo) {
+        Optional<Profumo> profumoToUpdate = profumoService.updateProfumoId(id, profumo);
+        if (profumoToUpdate.isPresent()) {
             return ResponseEntity.ok().body(profumoToUpdate.get());
         }
         return ResponseEntity.notFound().build();
@@ -66,12 +66,36 @@ public class ProfumoController {
      * @param id con l'id cerca i profumi
      * @return mostra i profumi elminati se ci sono altrimenti mostra "not found"
      */
-    @DeleteMapping ("/delete/{id}")
-    public ResponseEntity<Profumo> deleteProfumo (@PathVariable Long id){
-        Optional <Profumo> deleteProfumo = profumoService.deleteProfumo(id);
-        if (deleteProfumo.isPresent()){
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Profumo> deleteProfumo(@PathVariable Long id) {
+        Optional<Profumo> deleteProfumo = profumoService.deleteProfumo(id);
+        if (deleteProfumo.isPresent()) {
             return ResponseEntity.ok().body(deleteProfumo.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    /**
+     * @return lista profumi con record status attivo
+     */
+    @GetMapping("/active")
+    public ResponseEntity<List<Profumo>> findByActive() {
+        Optional<List<Profumo>> listaProfumi = profumoService.getByRecordStatusActive();
+        if (listaProfumi.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(listaProfumi.get());
+    }
+
+    /**
+     * @return lista profumi con record status inattivo
+     */
+    @GetMapping("/inactive")
+    public ResponseEntity<List<Profumo>> findByInactive() {
+        Optional<List<Profumo>> listaProfumi = profumoService.getByRecordStatusInactive();
+        if (listaProfumi.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(listaProfumi.get());
     }
 }
--- src/main/java/com/team3/essence/lavib/essence_lab/services/ProfumoService.java
+++ src/main/java/com/team3/essence/lavib/essence_lab/services/ProfumoService.java
@@ -1,5 +1,6 @@
 package com.team3.essence.lavib.essence_lab.services;
 
+import com.team3.essence.lavib.essence_lab.Enum.RecordStatusEnum;
 import com.team3.essence.lavib.essence_lab.entities.Essenza;
 import com.team3.essence.lavib.essence_lab.entities.Profumo;
 import com.team3.essence.lavib.essence_lab.repositories.ProfumoRepository;
@@ -13,41 +14,38 @@ import java.util.Optional;
 public class ProfumoService {
     @Autowired
     private ProfumoRepository profumoRepository;
+
     /**
-     *
      * @param profumo
      * @return Salva l'oggetto e lo ritorna;
      */
-    public Profumo addProfumo(Profumo profumo){
+    public Profumo addProfumo(Profumo profumo) {
         return profumoRepository.save(profumo);
     }
 
     /**
-     *
      * @return Ritorna la lista dei profumi;
      */
-    public List<Profumo> getAllProfumi(){
+    public List<Profumo> getAllProfumi() {
         return profumoRepository.findAll();
     }
 
     /**
-     *
      * @param id
      * @return Ritorna il profumo cercato tramite id;
      */
-    public Optional<Profumo> getProfumoId(Long id){
+    public Optional<Profumo> getProfumoId(Long id) {
         return profumoRepository.findById(id);
     }
 
     /**
-     *
-     * @param id per cercare l'essenza da aggiornare
+     * @param id              per cercare l'essenza da aggiornare
      * @param profumoToUpdate
      * @return L'essenza aggiornata o un oggetto vuoto se non è presente
      */
-    public Optional<Profumo> updateProfumoId(Long id,Profumo profumoToUpdate){
+    public Optional<Profumo> updateProfumoId(Long id, Profumo profumoToUpdate) {
         Optional<Profumo> profumoUpdate = profumoRepository.findById(id);
-        if(profumoUpdate.isPresent()){
+        if (profumoUpdate.isPresent()) {
             profumoUpdate.get().setAllergeni_profumo(profumoToUpdate.getAllergeni_profumo());
             profumoUpdate.get().setDescrizione_profumo(profumoToUpdate.getDescrizione_profumo());
             profumoUpdate.get().setIngredienti_profumo(profumoToUpdate.getIngredienti_profumo());
@@ -59,23 +57,40 @@ public class ProfumoService {
             profumoUpdate.get().setEssenze(profumoToUpdate.getEssenze());
             Profumo profumoUpdated = profumoRepository.save(profumoUpdate.get());
             return Optional.of(profumoUpdated);
-        }else{
+        } else {
             return Optional.empty();
         }
     }
 
     /**
-     *
      * @param id per cercare il profumo da eliminare
      * @return il profumo eliminato o un oggetto vuoto se non esiste
      */
-    public Optional<Profumo> deleteProfumo(Long id){
+    public Optional<Profumo> deleteProfumo(Long id) {
         Optional<Profumo> deleteProfumo = profumoRepository.findById(id);
-        if(deleteProfumo.isPresent()){
+        if (deleteProfumo.isPresent()) {
             profumoRepository.delete(deleteProfumo.get());
-        }else{
+        } else {
             return Optional.empty();
         }
         return deleteProfumo;
+    }
+
+    /**
+     *
+     * @return lista profumi con recordstatus attivo
+     */
+    public Optional<List<Profumo>> getByRecordStatusActive() {
+        Optional<List<Profumo>> listaProfumi = Optional.ofNullable(profumoRepository.findByRecordStatus(RecordStatusEnum.A));
+        return listaProfumi;
+    }
+
+    /**
+     *
+     * @return lista profumi con recordstatus inattivo
+     */
+    public Optional<List<Profumo>> getByRecordStatusInactive() {
+        Optional<List<Profumo>> listaProfumi = Optional.ofNullable(profumoRepository.findByRecordStatus(RecordStatusEnum.I));
+        return listaProfumi;
     }
 }
